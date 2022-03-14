4ba4960d6047835edf442f57c2bb66ffd9d06bf3
t-hoshino206 <21st.century.eccentric.boy.f06@gmail.com>
2022-03-14 12:51:48 +0900

    deviseのインストール

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope :users do
+    get '/users', to: redirect("/users/sign_up")
+  end
+  
   root "items#index"
   resources :items, only: :index
 end
