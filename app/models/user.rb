f7a829fa606952f829ae0d174182582edf1a4bc7
t-hoshino206 <21st.century.eccentric.boy.f06@gmail.com>
2022-03-14 18:58:21 +0900

    バリデーションの追加修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :password, format: { with: VALID_PASSWORD_REGEX }
-  validates :last_name, presence: true
-  validates :first_name, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/ }
   validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
   validates :birth_date, presence: true
