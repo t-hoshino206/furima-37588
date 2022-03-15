dd4b9b3ab14fae0627124af158f84902811bc64a
t-hoshino206 <21st.century.eccentric.boy.f06@gmail.com>
2022-03-15 12:47:24 +0900

    Active Hashの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,13 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :prefecrure
+  belongs_to_active_hash :shipping_fee
+  belongs_to_active_hash :shipping_schedule
+
   validates :name, presence: true
   validates :description, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :shipping_fee_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :shipping_schedule_id, presence: true
   validates :price, presence: true
 
   belongs_to :user
