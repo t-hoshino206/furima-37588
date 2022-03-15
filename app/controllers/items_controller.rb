d2ffca219255dd8f05bdd062d56d7e59b022916e
t-hoshino206 <21st.century.eccentric.boy.f06@gmail.com>
2022-03-15 12:21:26 +0900

    アソシエーション定義、ストロングパラメーター設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,4 +9,8 @@ class ItemsController < ApplicationController
 
   def create
   end
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_schedule_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
