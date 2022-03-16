08448189636edd4d1b4a29d6adeb45d6ac56b597
t-hoshino206 <21st.century.eccentric.boy.f06@gmail.com>
2022-03-16 19:03:40 +0900

    コードレビュー任意修正箇所の反映

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,9 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if @item.user == current_user
-      render "edit"
-    else
+    if @item.user != current_user
       redirect_to root_path
     end
   end
